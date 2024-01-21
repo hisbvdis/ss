e2a2668e3d24a4601c0c6c92c57b42da0556de84
Roman Zvarich <hisbvdis@gmail.com>
2024-01-21 15:11:56 +0200

    changes

--- drizzle/client.drizzle.js
+++
@@ -1,6 +0,0 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-
-const connectionString = "postgresql://postgres:postgres@localhost:5432/ssd";
-const queryClient = postgres(connectionString)
-export const db = drizzle(queryClient);
