aa5893e51f691fc361aaa48748b70a061b4b7b96
Roma Zvarych <hisbvdis@gmail.com>
2024-02-18 19:53:42 +0200

    InputAddon Component TS migrating

---
+++ app/_components/InputAddon/InputAddon.tsx
@@ -0,0 +1,16 @@
+import styles from "./InputAddon.module.css";
+
+
+export default function InputAddon(props:Props) {
+  const { children } = props;
+
+  return (
+    <span className={styles["inputAddon"]}>
+      {children}
+    </span>
+  )
+}
+
+interface Props {
+  children: React.ReactNode;
+}
