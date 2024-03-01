325627b4dccd968f73c3155a27de1e6d84794fda
Roma Zvarych <hisbvdis@gmail.com>
2024-03-01 13:09:27 +0200

    description

--- app/_pages/Object/Description/Description.jsx
+++ app/_pages/Object/Description/Description.jsx
@@ -1,19 +1,34 @@
-import { useContext } from "react";
+"use client";
+import { useContext, useEffect, useRef, useState } from "react";
 // -----------------------------------------------------------------------------
 import { Card } from "@/app/_components/Card/";
-// -----------------------------------------------------------------------------
 import { ObjectContext } from "../Object";
+// -----------------------------------------------------------------------------
+import styles from "./styles.module.css";
+import clsx from "clsx";
 
 
 export default function Description() {
   const { state } = useContext(ObjectContext);
+  if (!state.description) return null;
+  const textRef = useRef();
+  const [ isTrimText, setIsTrimText ] = useState(false);
+  const [ isShowFullText, setIsShowFullText ] = useState(false);
 
-  return (<>
-    {state.description
-      ? <Card style={{whiteSpace: "pre-line"}}>
-          <Card.Section>{state.description}</Card.Section>
-        </Card>
-      : null
+  useEffect(() => {
+    if (textRef.current.scrollHeight > textRef.current.clientHeight) {
+      setIsTrimText(true);
     }
-  </>)
+  }, [])
+
+  return (
+    <Card className={styles["desc"]}>
+      <p className={clsx(styles["desc__content"], !isShowFullText && styles["desc__content--short"])} ref={textRef}>
+        {state.description}
+      </p>
+      {isTrimText && <button onClick={() => setIsShowFullText(!isShowFullText)}>
+        {isShowFullText ? "Меньше" : "Больше"}
+      </button>}
+    </Card>
+  )
 }
